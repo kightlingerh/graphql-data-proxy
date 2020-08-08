5bb927e52011158bb4895791c41afef0eb7ba808
Harry Kightlinger <kightlingerh@gmail.com>
2020-08-08 11:48:08 -0700

    Added tests for refs reactivity

--- test/cache/reactive.ts
+++ test/cache/reactive.ts
@@ -1,9 +1,10 @@
-import { computed } from '@vue/reactivity'
+import { computed, shallowRef } from '@vue/reactivity'
 import * as assert from 'assert'
 import { isRight } from 'fp-ts/lib/Either'
-import { pipe } from 'fp-ts/lib/function'
-import { chain, fromEither, rightIO } from 'fp-ts/lib/IOEither'
-import { isSome, none } from 'fp-ts/lib/Option'
+import { constant, pipe } from 'fp-ts/lib/function'
+import { chain, fold, fromEither, right, rightIO } from 'fp-ts/lib/IOEither'
+import { getOrElse, isNone, isSome, none, some } from 'fp-ts/lib/Option'
+import { constEmptyArray } from '../../src/shared'
 import { make } from '../../src/cache/Cache'
 import * as M from '../../src/model/index'
 import * as N from '../../src/node/index'
@@ -14,7 +15,8 @@ const PersonalInfoNode = N.type('PersonalInfo', {
 	pictureUrl: N.option(N.staticString),
 	firstName: N.staticString,
 	lastName: N.staticString,
-	highSchool: N.option(N.staticString)
+	highSchool: N.option(N.staticString),
+	parents: N.markAsEntity(N.array(N.staticString))
 })
 
 const PersonNode = N.type('Person', {
@@ -28,19 +30,21 @@ const Person1: N.TypeOf<typeof PersonNode> = {
 		pictureUrl: none,
 		firstName: 'Harry',
 		lastName: 'Kightlinger',
-		highSchool: none
+		highSchool: none,
+		parents: []
 	}
 }
 
 const Person1Update: N.TypeOfPartial<typeof PersonNode> = {
 	id: '1',
 	personalInfo: {
-		lastName: 'Baldwin'
-		// highSchool: some('La Canada High School')
+		lastName: 'Baldwin',
+		highSchool: some('La Canada High School'),
+		parents: ['Jeff', 'Barbara']
 	}
 }
 
-const Person1Final: N.TypeOfPartial<typeof PersonNode> = {
+const Person1Final: N.TypeOf<typeof PersonNode> = {
 	id: '1',
 	personalInfo: {
 		...Person1.personalInfo,
@@ -53,18 +57,24 @@ describe('reactivity', () => {
 		const cache = make({})(PersonNode)(PersonNode)
 		assert.deepStrictEqual(isRight(cache), true)
 
+		const read = pipe(
+			fromEither(cache),
+			chain((c) => rightIO(c.read({})))
+		)
+
+		const cacheResult = computed(read)
+
+		// cache read is none
+
+		assert.deepStrictEqual(isRight(cacheResult.value) && isNone(cacheResult.value.right), true)
+
 		// initial write
 		pipe(
 			fromEither(cache),
 			chain((c) => rightIO(c.write({})(Person1)))
 		)()
 
-		const read = pipe(
-			fromEither(cache),
-			chain((c) => rightIO(c.read({})))
-		)
-		const cacheResult = computed(read)
-
+		// cache value is updated
 		assert.deepStrictEqual(
 			isRight(cacheResult.value) && isSome(cacheResult.value.right) && cacheResult.value.right.value,
 			Person1
@@ -80,5 +90,39 @@ describe('reactivity', () => {
 			isRight(cacheResult.value) && isSome(cacheResult.value.right) && cacheResult.value.right.value,
 			Person1Final
 		)
-	})
+	}),
+		it('toRefs should return reactive objects', () => {
+			const cache = make({})(PersonNode)(PersonNode)
+			assert.deepStrictEqual(isRight(cache), true)
+
+			const readParents = pipe(
+				fromEither(cache),
+				chain((c) => rightIO(c.toRefs({}))),
+				chain((refs) => right(refs.personalInfo.parents)),
+				fold(constant(constant(shallowRef(none))), (parentsRef) => () => parentsRef)
+			)
+
+			const parents = computed(() => pipe(readParents().value, getOrElse<string[]>(constEmptyArray)))
+
+			// parents is empty
+
+			assert.deepStrictEqual(parents.value, [])
+
+			// initial write
+			pipe(
+				fromEither(cache),
+				chain((c) => rightIO(c.write({})(Person1)))
+			)()
+
+			// cache value is updated
+			assert.deepStrictEqual(parents.value, Person1.personalInfo.parents)
+
+			//update
+			pipe(
+				fromEither(cache),
+				chain((c) => rightIO(c.write({})(Person1Update)))
+			)()
+
+			assert.deepStrictEqual(parents.value, Person1Final.personalInfo.parents)
+		})
 })
